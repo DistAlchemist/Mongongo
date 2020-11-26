ff194576148e04f93172689d955c9027c3d88aa2
Qizhou Guo <guoqizhou19@gmail.com>
2020-11-26 13:22:44 +0000

    build ast from listener! >.<

--- mql/mqldriver.go
+++ mql/mqldriver.go
@@ -4,119 +4,10 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/DistAlchemist/Mongongo/mql/parser"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
+
+	"github.com/DistAlchemist/Mongongo/mql/parser"
 )
-
-type mqlListener struct {
-	*parser.BaseMqlListener
-	stmtType                int
-	action                  string
-	dimensionCnt            int
-	tableName               string
-	columnFamilyName        string
-	rowKey                  string
-	columnOrSuperColumnKeys []string
-	cellValue               string
-}
-
-// // VisitTerminal is called when a terminal node is visited.
-// func (l *mqlListener) VisitTerminal(node antlr.TerminalNode) {
-// 	fmt.Println("visitTerminal")
-// }
-
-// // VisitErrorNode is called when an error node is visited.
-// func (l *mqlListener) VisitErrorNode(node antlr.ErrorNode) {
-// 	fmt.Println("visitErrorNode")
-// }
-
-// // EnterStringVal is called when production stringVal is entered.
-// func (l *mqlListener) EnterStringVal(ctx *parser.StringValContext) {
-// 	fmt.Println("enterstringVal")
-// }
-
-// func (l *mqlListener) EnterEveryRule(c antlr.ParserRuleContext) {
-// 	fmt.Println("enterEveryRule")
-// }
-
-// // ExitStmt is called when production stmt is exited.
-// func (l *mqlListener) ExitStmt(c *parser.StmtContext) {
-// 	fmt.Println("exitStmt")
-// }
-
-func (l *mqlListener) EnterStmt(c *parser.StmtContext) {
-	log.Println("entering EnterStmt")
-}
-
-func (l *mqlListener) EnterSetStmt(c *parser.SetStmtContext) {
-	l.stmtType = parser.MqlParserRULE_setStmt
-	l.action = "SET"
-	log.Println("entering EnterSetStmt")
-}
-
-func (l *mqlListener) EnterGetStmt(c *parser.GetStmtContext) {
-	l.stmtType = parser.MqlParserRULE_getStmt
-	l.action = "GET"
-	log.Println("entering EnterGetStmt")
-}
-
-func (l *mqlListener) EnterColumnSpec(c *parser.ColumnSpecContext) {
-	fmt.Println("enter columnSpec")
-	// skip tableName, columnFamilyName and rowKey
-	l.dimensionCnt = c.GetChildCount() - 3
-	fmt.Println("ColumnSpec childCount: %v", c.GetChildCount())
-	fmt.Println("dimensionCnt: %v", l.dimensionCnt)
-
-	// if l.dimensionCnt > 1 {
-	// 	log.Fatal("currenty only support tableName.columnFamilyName[rowkey][column]=cellValue")
-	// }
-	// if l.dimensionCnt > 2 {
-	// 	log.Fatal("too many dimensions")
-	// }
-}
-
-func (l *mqlListener) ExitColumnSpec(c *parser.ColumnSpecContext) {
-	fmt.Println("exit columnSpec")
-	// skip tableName, columnFamilyName and rowKey
-	l.dimensionCnt = c.GetChildCount() - 3
-	fmt.Println("exit ColumnSpec childCount: %v", c.GetChildCount())
-	fmt.Println("exit dimensionCnt: %v", l.dimensionCnt)
-
-	// if l.dimensionCnt > 1 {
-	// 	log.Fatal("currenty only support tableName.columnFamilyName[rowkey][column]=cellValue")
-	// }
-	// if l.dimensionCnt > 2 {
-	// 	log.Fatal("too many dimensions")
-	// }
-}
-func (l *mqlListener) EnterTableName(c *parser.TableNameContext) {
-	fmt.Println("enter tableName")
-	l.tableName = c.GetText()
-}
-
-func (l *mqlListener) EnterColumnFamilyName(c *parser.ColumnFamilyNameContext) {
-	fmt.Println("enter columnFamilyName")
-	l.columnFamilyName = c.GetText()
-}
-
-func (l *mqlListener) EnterRowKey(c *parser.RowKeyContext) {
-	fmt.Println("enter rowKey")
-	l.rowKey = c.GetText()
-}
-
-func (l *mqlListener) EnterColumnOrSuperColumnKey(c *parser.ColumnOrSuperColumnKeyContext) {
-	fmt.Println("enter columnOrSuperColumnKey")
-	l.columnOrSuperColumnKeys = append(l.columnOrSuperColumnKeys, c.GetText())
-}
-
-// func (l *mqlListener) EnterValueExpr(c *parser.ValueExprContext) {
-// 	//
-// }
-
-func (l *mqlListener) EnterCellValue(c *parser.CellValueContext) {
-	fmt.Println("enter cellValue")
-	l.cellValue = c.GetText()
-}
 
 // Result embeds error message and results
 type Result struct {
@@ -136,7 +27,10 @@ func ExecuteQuery(query string) Result {
 	p := parser.NewMqlParser(stream)
 	// finally parse the expression (by walking the tree)
 	var listener mqlListener
+	listener.init()
 	antlr.ParseTreeWalkerDefault.Walk(&listener, p.Stmt())
+	log.Printf("travse the ast manually...\n")
+	listener.root.dfs()
 	fmt.Println("this is executeQuery function")
 	fmt.Printf("%+v\n", listener)
 	var res Result
--- mql/mqllistener.go
+++ mql/mqllistener.go
@@ -1,1 +1,137 @@
 package mql
+
+import (
+	"log"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+
+	"github.com/DistAlchemist/Mongongo/mql/parser"
+)
+
+type mqlListener struct {
+	*parser.BaseMqlListener
+	stmtType                int
+	action                  string
+	dimensionCnt            int
+	tableName               string
+	columnFamilyName        string
+	rowKey                  string
+	columnOrSuperColumnKeys []string
+	cellValue               string
+	root                    *node
+	curNode                 *node
+}
+
+type node struct {
+	id       int
+	text     string
+	parent   *node
+	children []*node
+}
+
+func (n *node) addChild(id int, text string) *node {
+	child := &node{id: id, text: text, parent: n}
+	n.children = append(n.children, child)
+	return child
+}
+
+func (n *node) dfs() {
+	log.Printf("enter id: %2v, text: %v\n", n.id, n.text)
+	for _, c := range n.children {
+		c.dfs()
+	}
+	log.Printf("exit  id: %2v, text: %v\n", n.id, n.text)
+}
+
+func (l *mqlListener) init() {
+	l.root = &node{-1, "", nil, nil}
+	l.curNode = l.root
+}
+
+// EnterEveryRule is called when any rule is entered.
+func (l *mqlListener) EnterEveryRule(c antlr.ParserRuleContext) {
+	log.Printf("Enter: GetRuleIndex: %v, ", c.GetRuleIndex())
+	log.Printf("GetText %v\n", c.GetText())
+	l.curNode = l.curNode.addChild(c.GetRuleIndex(), c.GetText())
+}
+
+// ExitEveryRule is called when any rule is exited.
+func (l *mqlListener) ExitEveryRule(c antlr.ParserRuleContext) {
+	log.Printf("Enter: GetRuleIndex: %v, ", c.GetRuleIndex())
+	log.Printf("GetText %v\n", c.GetText())
+	l.curNode = l.curNode.parent
+}
+
+// func (l *mqlListener) EnterStmt(c *parser.StmtContext) {
+// 	log.Println("entering EnterStmt")
+// }
+
+// func (l *mqlListener) EnterSetStmt(c *parser.SetStmtContext) {
+// 	l.stmtType = parser.MqlParserRULE_setStmt
+// 	l.action = "SET"
+// 	log.Println("entering EnterSetStmt")
+// }
+
+// func (l *mqlListener) EnterGetStmt(c *parser.GetStmtContext) {
+// 	l.stmtType = parser.MqlParserRULE_getStmt
+// 	l.action = "GET"
+// 	log.Println("entering EnterGetStmt")
+// }
+
+// func (l *mqlListener) EnterColumnSpec(c *parser.ColumnSpecContext) {
+// 	fmt.Println("enter columnSpec")
+// 	// skip tableName, columnFamilyName and rowKey
+// 	l.dimensionCnt = c.GetChildCount() - 3
+// 	fmt.Println("ColumnSpec childCount: %v", c.GetChildCount())
+// 	fmt.Println("dimensionCnt: %v", l.dimensionCnt)
+
+// 	// if l.dimensionCnt > 1 {
+// 	// 	log.Fatal("currenty only support tableName.columnFamilyName[rowkey][column]=cellValue")
+// 	// }
+// 	// if l.dimensionCnt > 2 {
+// 	// 	log.Fatal("too many dimensions")
+// 	// }
+// }
+
+// func (l *mqlListener) ExitColumnSpec(c *parser.ColumnSpecContext) {
+// 	fmt.Println("exit columnSpec")
+// 	// skip tableName, columnFamilyName and rowKey
+// 	l.dimensionCnt = c.GetChildCount() - 3
+// 	fmt.Println("exit ColumnSpec childCount: %v", c.GetChildCount())
+// 	fmt.Println("exit dimensionCnt: %v", l.dimensionCnt)
+
+// 	// if l.dimensionCnt > 1 {
+// 	// 	log.Fatal("currenty only support tableName.columnFamilyName[rowkey][column]=cellValue")
+// 	// }
+// 	// if l.dimensionCnt > 2 {
+// 	// 	log.Fatal("too many dimensions")
+// 	// }
+// }
+// func (l *mqlListener) EnterTableName(c *parser.TableNameContext) {
+// 	fmt.Println("enter tableName")
+// 	l.tableName = c.GetText()
+// }
+
+// func (l *mqlListener) EnterColumnFamilyName(c *parser.ColumnFamilyNameContext) {
+// 	fmt.Println("enter columnFamilyName")
+// 	l.columnFamilyName = c.GetText()
+// }
+
+// func (l *mqlListener) EnterRowKey(c *parser.RowKeyContext) {
+// 	fmt.Println("enter rowKey")
+// 	l.rowKey = c.GetText()
+// }
+
+// func (l *mqlListener) EnterColumnOrSuperColumnKey(c *parser.ColumnOrSuperColumnKeyContext) {
+// 	fmt.Println("enter columnOrSuperColumnKey")
+// 	l.columnOrSuperColumnKeys = append(l.columnOrSuperColumnKeys, c.GetText())
+// }
+
+// // func (l *mqlListener) EnterValueExpr(c *parser.ValueExprContext) {
+// // 	//
+// // }
+
+// func (l *mqlListener) EnterCellValue(c *parser.CellValueContext) {
+// 	fmt.Println("enter cellValue")
+// 	l.cellValue = c.GetText()
+// }
